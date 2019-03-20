f920092ed7b21217164210393bcfd9eac24148cf
Gram <master_fess@mail.ru>
2019-03-20 16:20:02 +0300

    +code

---
+++ dephell_specifier/__init__.py
@@ -0,0 +1,6 @@
+from .git_specifier import GitSpecifier
+from .range_specifier import RangeSpecifier
+from .specifier import Specifier
+
+
+__all__ = ['GitSpecifier', 'RangeSpecifier', 'Specifier']
---
+++ dephell_specifier/constants.py
@@ -0,0 +1,14 @@
+# built-in
+from enum import Enum, unique
+
+
+@unique
+class JoinTypes(Enum):
+    AND = 1
+    OR = 2
+
+
+PYTHONS_DEPRECATED = ('2.6', '2.7', '3.0', '3.1', '3.2', '3.3', '3.4')
+PYTHONS_POPULAR = ('3.5', '3.6', '3.7')
+PYTHONS_UNRELEASED = ('3.8', '4.0')
+PYTHONS = PYTHONS_POPULAR + PYTHONS_DEPRECATED + PYTHONS_UNRELEASED
