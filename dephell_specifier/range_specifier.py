5b56aed237fa8608859899c5b409c701803311ba
Gram <master_fess@mail.ru>
2020-04-27 18:48:44 +0200

    suppress new mypy errors

--- dephell_specifier/range_specifier.py
+++ dephell_specifier/range_specifier.py
@@ -120,7 +120,7 @@ class RangeSpecifier:
             return Specifier(constr.replace('.*', '.0'))
 
         version = parse(constr.lstrip(OPERATOR_SYMBOLS).rstrip('.*'))
-        parts = version.release[:-1] + (version.release[-1] + 1, )
+        parts = version.release[:-1] + (version.release[-1] + 1, )  # type: ignore
         return Specifier(constr[:2] + '.'.join(map(str, parts)))
 
     @staticmethod
@@ -220,7 +220,7 @@ class RangeSpecifier:
             elif left is None or python > left:
                 excluded.append(python)
         if right is not None:
-            right = (pythons + [None])[pythons.index(right) + 1]
+            right = (pythons + [None])[pythons.index(right) + 1]  # type: ignore
 
         # get excluded intervals
         if right is not None:
