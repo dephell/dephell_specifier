ceef113cde60e5fdff10fccb61d84128d7af35c1
gram <git@orsinium.dev>
2022-12-17 08:42:43 +0100

    migrate to flit

--- dephell_specifier/__init__.py
+++ dephell_specifier/__init__.py
@@ -1,6 +1,12 @@
+"""A package to work with version specifiers.
+
+Supports PEP-440, SemVer, Ruby, NPM, and Maven specifier formats.
+"""
+# app
 from .git_specifier import GitSpecifier
 from .range_specifier import RangeSpecifier
 from .specifier import Specifier
 
 
+__version__ = '0.2.2'
 __all__ = ['GitSpecifier', 'RangeSpecifier', 'Specifier']
