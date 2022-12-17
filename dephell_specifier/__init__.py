62830ddc4dd6ab52a657b4600282b1abb6385911
gram <git@orsinium.dev>
2022-12-17 09:18:17 +0100

    bump version

--- dephell_specifier/__init__.py
+++ dephell_specifier/__init__.py
@@ -9,5 +9,5 @@ from .range_specifier import RangeSpecifier
 from .specifier import Specifier
 
 
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 __all__ = ['GitSpecifier', 'RangeSpecifier', 'Specifier']
